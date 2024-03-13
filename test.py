5ceaffb2bf874c46ed759332677a025898a60196
bgarlow <brentgarlow@gmail.com>
2024-03-13 15:02:02 -0600

    Added HTTP Post test

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
 import os
+import requests 
+
+print("Running the python script");
+
 githubToken = os.environ['GITHUB_TOKEN']
-print("Github Token: ", githubToken)
+githubApiUrl = "https://api.github.com/repos/bgarlow/auth0-express-webapp-sample/issues"
+
+issueBody = {
+    "title": "This is an issue submitted by the python script",
+    "body": "this is the body",
+    "assignees": ["bgarlow"],
+    "labels": ["bug"]
+}
+
+response = requests.post(githubApiUrl, issueBody)
+
+print(response.text)

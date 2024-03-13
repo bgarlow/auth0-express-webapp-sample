3413665dc8b5d4b27460636c62faea9bd427f890
bgarlow <brentgarlow@gmail.com>
2024-03-13 11:18:20 -0600

    First commit

--- 01-Login/server.js
+++ 01-Login/server.js
@@ -57,3 +57,22 @@ http.createServer(app)
   .listen(port, () => {
     console.log(`Listening on ${config.baseURL}`);
   });
+
+  process.on('SIGINT', shutdownGracefully);
+  process.on('SIGTERM', shutdownGracefully);
+
+  function shutdownGracefully() {
+    console.log('\nShutting down gracefully...');
+
+    server.close(() => {
+      console.log('Server closed.');
+      // Close any other connections or resources here
+      process.exit(0);
+    });
+  
+    // Force close the server after 5 seconds
+    setTimeout(() => {
+      console.error('Could not close connections in time, forcefully shutting down');
+      process.exit(1);
+    }, 5000);
+  }

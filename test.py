9356f67e1ce5728ca1afe8908ca2f0a228b34133
bgarlow <brentgarlow@gmail.com>
2024-03-15 11:37:46 -0600

    Updated test.py to read the json file

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import os
-import requests 
+import requests
+import json
 
 githubToken = os.environ['GITHUB_TOKEN']
 githubApiUrl = "https://api.github.com/repos/bgarlow/auth0-express-webapp-sample/issues"
 
 print("Running the python script test.py")
 print("Endpoint URL: ", githubApiUrl)
+
+with open("vuln.json") as file:
+    data = json.load(file)
+
+print("data: ", data)
 
 authHeaderValue = "Bearer " + githubToken
 

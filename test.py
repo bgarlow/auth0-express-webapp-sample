9199e91a2f8675817de6dc81f0db56a4f60e05a9
bgarlow <brentgarlow@gmail.com>
2024-03-15 12:50:54 -0600

    fixed path

--- test.py
+++ test.py
@@ -9,7 +9,9 @@ githubApiUrl = "https://api.github.com/repos/bgarlow/auth0-express-webapp-sample
 print("Running the python script test.py")
 print("Endpoint URL: ", githubApiUrl)
 
-with open(workingDirectory + "/vuln.json") as file:
+jsonFilePath = workingDirectory + "/vuln.json"
+
+with open(jsonFilePath) as file:
     data = json.load(file)
 
 print("data: ", data)
